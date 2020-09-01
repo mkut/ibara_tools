09c5bae3b4263c35cf5206fa37d3da5d73eddfb7
Mirai Towa <mkut.at@gmail.com>
2020-09-01 19:46:35 +0900

    new goods

--- js/mikiri_sim/src/fixtures.js
+++ js/mikiri_sim/src/fixtures.js
@@ -31,4 +31,9 @@ export const shopItems = [
    { name: 'お野菜', type: '食材', price: 50, special: false, power: 10, effect1: { name: '器用', lv: 10, reqlv: 15 }, effect2: { name: '幸運', lv: 10, reqlv: 25 }, effect3: { name: '命脈', lv: 10, reqlv: 35 } },
    { name: '良いお野菜', type: '食材', price: 200, special: false, power: 20, effect1: { name: '器用', lv: 20, reqlv: 20 }, effect2: { name: '幸運', lv: 20, reqlv: 30 }, effect3: { name: '命脈', lv: 20, reqlv: 40 } },
    { name: 'すごいお野菜', type: '食材', price: 400, special: false, power: 30, effect1: { name: '器用', lv: 30, reqlv: 25 }, effect2: { name: '幸運', lv: 30, reqlv: 35 }, effect3: { name: '命脈', lv: 30, reqlv: 45 } },
+   { name: '生命の宝石', type: '素材', price: 800, special: false, power: 30, effect1: { name: '快癒', lv: 30, reqlv: 70}, effect2: { name: '活力', lv: 30, reqlv: 70}, effect3: { name: '耐疫', lv: 30, reqlv: 70} },
+   { name: '精神の宝石', type: '素材', price: 800, special: false, power: 30, effect1: { name: '魔力', lv: 30, reqlv: 70}, effect2: { name: '気合', lv: 30, reqlv: 70}, effect3: { name: '耐狂', lv: 30, reqlv: 70} },
+   { name: '乱流の宝石', type: '素材', price: 800, special: false, power: 30, effect1: { name: '連撃', lv: 30, reqlv: 70}, effect2: { name: '反撃', lv: 30, reqlv: 70}, effect3: { name: '耐災', lv: 30, reqlv: 70} },
+   { name: '朝凪の宝石', type: '素材', price: 800, special: false, power: 30, effect1: { name: '共鳴', lv: 30, reqlv: 70}, effect2: { name: '回復', lv: 30, reqlv: 70}, effect3: { name: '反射', lv: 30, reqlv: 70} },
+   { name: '夕凪の宝石', type: '素材', price: 800, special: false, power: 30, effect1: { name: '心酔', lv: 30, reqlv: 70}, effect2: { name: '舞祝', lv: 30, reqlv: 70}, effect3: { name: '充填', lv: 30, reqlv: 70} },
 ];
